175bca39639e805513e44fff11f0693d82467489
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-09 10:08:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if amount < sender.balance
+    if (valid? == true && amount < sender.balance)
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
