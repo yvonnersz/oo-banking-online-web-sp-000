1aa9e90904049e83ce6f1c06b581a760bd590481
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-09 09:31:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,9 @@ class Transfer
   attr_accessor :sender, :receiver, :amount
 
   def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
     @status = "pending"
 
 
