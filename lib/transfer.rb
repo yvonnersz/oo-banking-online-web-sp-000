6a6e8d02483667613788861da7bc4c909aacee40
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-09 10:17:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,12 +18,11 @@ class Transfer
     if (valid? == true && amount < sender.balance)
       counter = 0
       until counter = 1
-        sender.balance -= amount
-        receiver.balance += amount
-        @status = "complete"
         counter +=1
-        binding.pry
       end
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = "complete"
     else
       "Transaction rejected. Please check your account balance."
     end
