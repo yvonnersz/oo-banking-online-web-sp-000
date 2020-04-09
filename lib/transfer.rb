ae619de5f19fc5aa8b0ffb98656e46d89a4cca80
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-09 09:52:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
-    else
+    elsif valid? == false
       "Transaction rejected. Please check your account balance."
       @status = "rejected"
     end
