f9ee61d5ead991f91f3574df10b548fd4bcc95e2
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-09 10:09:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       receiver.balance += amount
       @status = "complete"
     else
-      puts "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
     end
 
   end
