0a7a39b2bdf1b06c7e810448cb1f30e874be8453
Yvonne M Pham <yvonnersz@gmail.com>
2020-04-09 10:02:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? == true
+    if valid? == true && amount < sender.balance
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
